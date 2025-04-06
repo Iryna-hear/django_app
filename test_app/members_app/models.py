5792f016584c0fb479aae9ec83958c43ae195016
Ирина Кеда <irinakeda@MacBookAir.fritz.box>
2025-04-06 20:34:28 +0200

    Initial commit

--- test_app/members_app/models.py
+++ test_app/members_app/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Members(models.Model):
-    title = models.CharField(max_length=255)
-    descriptions = models.TextField(blank=True)
+      title = models.CharField(max_length=100)
+      description = models.TextField(blank=True)
+      User= models.ForeignKey(User, on_delete=models.CASCADE)
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+      completed = models.BooleanField(default=False)
 
 
-
-    def __str__(self):
+def __str__(self):
         return self.title
 
 
